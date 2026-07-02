389d845112c8906c69657d689162134bee1126e5
PAULO CESAR SCHMITT KRAS <61030967+PauloKras@users.noreply.github.com>
2026-07-02 10:25:33 -0300

    Commit 4

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,8 +6,14 @@ export default defineConfig({
   testDir: './tests',           // corrigido: era './specs'
   timeout: 30_000,
   retries: 1,
-  reporter: [['html', { open: 'on-failure' }], ['list']],
-
+  //reporter: [['html', { open: 'on-failure' }], ['list']],
+  reporter: [['html', { open: 'never' }], ['list']], // alterado 'on-failure' para 'never' na pipeline
+  
+  use: {
+    trace: 'retain-on-failure', // Isso vai gravar um raio-x do teste se ele falhar
+    screenshot: 'only-on-failure',
+  },
+  
   projects: [
     {
       name: 'api',
